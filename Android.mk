a7c37b7e6127695d9b136f9d98f1fffd9704b5de
jfdsmabalot <jfdsmabalot@gmail.com>
2013-12-19 20:18:02 +0800

    Strict Aliasing fix

--- Android.mk
+++ Android.mk
@@ -3,11 +3,11 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= ping.c ping_common.c
 LOCAL_MODULE := ping
-LOCAL_CFLAGS := -DWITHOUT_IFADDRS -Wno-sign-compare
+LOCAL_CFLAGS := -DWITHOUT_IFADDRS -Wno-sign-compare -fno-strict-aliasing
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
-LOCAL_CFLAGS := -DWITHOUT_IFADDRS -Wno-sign-compare
+LOCAL_CFLAGS := -DWITHOUT_IFADDRS -Wno-sign-compare -fno-strict-aliasing
 LOCAL_SRC_FILES := ping6.c ping_common.c
 LOCAL_MODULE := ping6
 LOCAL_C_INCLUDES := external/openssl/include
